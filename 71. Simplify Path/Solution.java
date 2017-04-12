36223061b2e22b970fc55f26c6c6f414a512c008
liu2048 <ccliu2015@gmail.com>
2017-04-12 10:42:45 -0400

    String.split()

--- 71. Simplify Path/Solution.java
+++ 71. Simplify Path/Solution.java
@@ -1,23 +1,21 @@
 public class Solution {
     public String simplifyPath(String path) {
+        String[] string_array = path.split("/");
         StringBuilder sb = new StringBuilder("/");
-        String rst = "/";
-        String[] strs = path.split("/");
+        String rst_str = "/";
         Stack<String> st = new Stack<>();
-        for (String str : strs){
-            if (str.equals("..")){
-                if (!st.isEmpty()) st.pop();
-            }
-            else if (!str.equals(".") && !str.equals("")) st.push(str);
+        for (String str : string_array){
+            if (str.equals("..")) if (!st.isEmpty()) st.pop(); // ".." means to return to upper level   
+            else if (!str.equals("") && !str.equals(".")) st.push(str);
         }
         for (String str : st) {
             sb.append(str);
             sb.append("/");
         }
         if (sb.length() > 1) {
-            sb.delete(sb.length() - 1, sb.length());
-            rst = sb.toString();
+            sb.deleteCharAt(sb.length() - 1); // likely, sb.delete(sb.length() - 1, sb.length())
+            rst_str = sb.toString();
         }
-        return rst;
+        return rst_str;
     }
 }
