bc65622db01e2689d5954ae098212545b444be5b
Cong Liu <drptau@gmail.com>
2018-04-26 10:45:14 -0400

    redo
    merge from tail

--- 88. Merge Sorted Array/Solution.java
+++ 88. Merge Sorted Array/Solution.java
@@ -1,22 +1,25 @@
-public class Solution {
-	// merge from tail
+class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int index_1 = m - 1;
-        int index_2 = n - 1;
-        int index = m + n - 1;
-        while (index_1 >= 0 && index_2 >= 0){
-            if (nums1[index_1] > nums2[index_2]) {
-                nums1[index--] = nums1[index_1--];
+        int index1 = m - 1;
+        int index2 =  n - 1;
+        int index = m+n-1;
+        while (index1 > -1 && index2 > -1) {
+            int x = nums1[index1];
+            int y = nums2[index2];
+            if (x > y) {
+                nums1[index] = x;
+                index1--;                
+            } else {
+                nums1[index] = y;
+                index2--;
             }
-            else {
-                nums1[index--] = nums2[index_2--];
-            }
-        } 
-        while (index_1 >= 0) {
-            nums1[index--] = nums1[index_1--];
+            index--;
         }
-        while (index_2 >= 0) {
-            nums1[index--] = nums2[index_2--];
+        while (index1 > -1) {
+            nums1[index--] = nums1[index1--];
+        }
+        while (index2>-1) {
+            nums1[index--] = nums2[index2--];
         }
     }
 }
