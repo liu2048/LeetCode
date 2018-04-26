800cbee93ba60dd38834476be4ab571d25f9dbee
Cong Liu <drptau@gmail.com>
2018-04-26 11:26:21 -0400

    redo

--- 100. Same Tree/Solution.java
+++ 100. Same Tree/Solution.java
@@ -7,11 +7,22 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
-public class Solution {
+class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if (p == null ^ q == null) return false; // either p or q is null
-        if (p == null && q == null) return true; // both p and q are null
-        if (p.val != q.val) return false; // neither p nor q is null
+        if (p == null ^ q == null) return false;
+        if (p == null && q == null) return true;
+        if (p.val != q.val) return false;
         return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 }
+
+/**
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+        if (p.val != q.val) return false;
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
+*/
