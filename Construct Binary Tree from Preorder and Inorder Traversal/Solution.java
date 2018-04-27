3e906dcc650ccfbe075485b4f4f8d9e1570b3a81
Cong Liu <drptau@gmail.com>
2018-04-27 16:15:30 -0400

    redo

--- Construct Binary Tree from Preorder and Inorder Traversal/Solution.java
+++ Construct Binary Tree from Preorder and Inorder Traversal/Solution.java
@@ -7,26 +7,28 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
-public class Solution {
-    int[] preorder;
-    int[] inorder;
+class Solution {
+    int[] inorder;  
+    int[] preorder;    
     Map<Integer, Integer> map;
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        this.preorder = preorder;
         this.inorder = inorder;
+        this.preorder = preorder;                
         this.map = new HashMap<>();
         int len = inorder.length;
-        for (int i = 0; i < len; i++) map.put(inorder[i], i);
+        for (int i = 0; i < len; i++) {
+            map.put(inorder[i], i);
+        }
         return helper(0, len-1, 0, len-1);
     }
-    private TreeNode helper(int preBegin, int preEnd, int inBegin, int inEnd){
+    public TreeNode helper(int preBegin, int preEnd, int inBegin, int inEnd) {
         if (preBegin > preEnd || inBegin > inEnd) return null;
         int val = preorder[preBegin];
         int index = map.get(val);
         int len = index - inBegin;
-        TreeNode root = new TreeNode(val);
-        root.left = helper(preBegin+1, preBegin+len, inBegin, index-1);
-        root.right = helper(preBegin+len+1, preEnd, index+1, inEnd);
-        return root;
+        TreeNode node = new TreeNode(val);
+        node.left  = helper(preBegin+1, preBegin+len, inBegin, index-1);
+        node.right = helper(preBegin+len+1, preEnd  , index+1, inEnd);
+        return node;
     }
 }
