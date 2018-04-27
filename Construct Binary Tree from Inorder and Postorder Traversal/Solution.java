67b8bf31dfb0cb11116b6c108c7a0b691eda2adc
Cong Liu <drptau@gmail.com>
2018-04-27 16:15:57 -0400

    redo

--- Construct Binary Tree from Inorder and Postorder Traversal/Solution.java
+++ Construct Binary Tree from Inorder and Postorder Traversal/Solution.java
@@ -7,26 +7,28 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
-public class Solution {
+class Solution {
     int[] inorder;
     int[] postorder;
     Map<Integer, Integer> map;
     public TreeNode buildTree(int[] inorder, int[] postorder) {
         this.inorder = inorder;
         this.postorder = postorder;
+        this.map = new HashMap<>();
         int len = inorder.length;
-        map = new HashMap<>();
-        for (int i = 0;  i < len; i++) map.put(inorder[i], i);
+        for (int i = 0; i < len; i++) {
+            map.put(inorder[i], i);
+        }
         return helper(0, len-1, 0, len-1);
     }
-    private TreeNode helper(int inBegin, int inEnd, int postBegin, int postEnd){
-        if (inBegin > inEnd || postBegin > postEnd) return null;
+    public TreeNode helper(int postBegin, int postEnd, int inBegin, int inEnd) {
+        if (postBegin > postEnd || inBegin > inEnd) return null;
         int val = postorder[postEnd];
         int index = map.get(val);
         int len = index - inBegin;
-        TreeNode root = new TreeNode(val);
-        root.left = helper(inBegin, index-1, postBegin, postBegin+len-1);
-        root.right = helper(index+1, inEnd,postBegin+len, postEnd-1);
-        return root;
+        TreeNode node = new TreeNode(val);
+        node.left  = helper(postBegin, postBegin+len-1, inBegin, index-1);
+        node.right = helper(postBegin+len, postEnd-1  , index+1,inEnd);        
+        return node;
     }
 }
