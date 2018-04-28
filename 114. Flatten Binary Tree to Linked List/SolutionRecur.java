5875fc8e2971cac3686f2aa40220c0986cfa8109
Cong Liu <drptau@gmail.com>
2018-04-28 11:41:24 -0400

    resursive solution

---
+++ 114. Flatten Binary Tree to Linked List/SolutionRecur.java
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public void flatten(TreeNode root) {
+        if (root == null) return;
+        if (root.left != null) flatten(root.left);
+        if (root.right != null) flatten(root.right);
+        TreeNode temp = root.right;
+        root.right = root.left;
+        root.left = null;
+        while (root.right != null) root = root.right;
+        root.right = temp;        
+    }
+}
