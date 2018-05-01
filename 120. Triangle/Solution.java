20a18ce07df0dd802ed231da8917ff2d4f63065a
Cong Liu <drptau@gmail.com>
2018-05-01 10:06:27 -0400

    redo
    dynamic programming

--- 120. Triangle/Solution.java
+++ 120. Triangle/Solution.java
@@ -1,19 +1,29 @@
-public class Solution {
+class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
-        int height = triangle.size();
-        int[] rst = new int[height];
-        List<Integer> lowestLevel = triangle.get(height-1);
-        int size = lowestLevel.size();
-        for (int i = 0; i < size; i++){
-            rst[i] = lowestLevel.get(i);
+        int depth = triangle.size();
+        List<Integer> lastRow = triangle.get(depth - 1);
+        int lastLen = lastRow.size();
+        int[] nums = new int[lastLen];
+        int count = 0;
+        for (int i : lastRow) {
+            nums[count] = i;
+            count++;
         }
-        for (int i = height-2; i > -1; i--){
-            size--;
-            List<Integer> curLevel = triangle.get(i);
-            for (int j = 0; j < size; j++){
-                rst[j] = curLevel.get(j) + Math.min(rst[j], rst[j+1]);
+        for (int i = depth-2; i >= 0; i--) {            
+            List<Integer> curRow = triangle.get(i);
+            List<Integer> lowRow = triangle.get(i+1);            
+            int size = curRow.size();
+            for (int j = 0; j < size; j++) {
+                int cur = curRow.get(j);
+                int lowLeft = nums[j];
+                int lowRight = nums[j+1];
+                if (lowLeft < lowRight) {
+                    nums[j] = lowLeft + cur;
+                } else {
+                    nums[j] = lowRight + cur;
+                }
             }
         }
-        return rst[0];
+        return nums[0];
     }
 }
