189dc1cdb4c027f185efe982af947f1bc14c5abc
Cong Liu <drptau@gmail.com>
2018-04-30 20:27:04 -0400

    redo

--- 118. Pascal's Triangle/Solution.java
+++ 118. Pascal's Triangle/Solution.java
@@ -1,14 +1,15 @@
-public class Solution {
+class Solution {
     public List<List<Integer>> generate(int numRows) {
         List<List<Integer>> rst = new LinkedList<>();
-        if (numRows < 1) return rst;
-        List<Integer> pre = new ArrayList<>();
-        pre.add(1);
-        rst.add(pre);
-        for (int i = 2; i <= numRows; i++){
+        if (numRows < 1) return rst;            
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        rst.add(list);        
+        List<Integer> pre = list;
+        for (int i = 2; i <= numRows; i++) {
             List<Integer> cur = new ArrayList<>();
             cur.add(1);
-            for (int j = 1; j < pre.size(); j++){
+            for (int j = 1; j <= i-2; j++) {
                 cur.add(pre.get(j-1) + pre.get(j));
             }
             cur.add(1);
