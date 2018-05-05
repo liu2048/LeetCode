cfe28c301c50103b6ef51c3d0314e22225cdebc2
Cong Liu <drptau@gmail.com>
2018-05-05 11:07:25 -0400

    deep copy

---
+++ 138. Copy List with Random Pointer/Solution.java
@@ -0,0 +1,32 @@
+// use map. time O(2n), space O(n)
+
+/**
+ * Definition for singly-linked list with a random pointer.
+ * class RandomListNode {
+ *     int label;
+ *     RandomListNode next, random;
+ *     RandomListNode(int x) { this.label = x; }
+ * };
+ */
+public class Solution {
+    public RandomListNode copyRandomList(RandomListNode head) {
+        Map<RandomListNode, RandomListNode> map = new HashMap<>();        
+        RandomListNode dummy = new RandomListNode(0);
+        RandomListNode q = dummy;
+        RandomListNode p = head;  
+        while (p != null) {
+            q.next = new RandomListNode(p.label);
+            q = q.next;
+            map.put(p, q);
+            p = p.next;            
+        }        
+        p = head;
+        q = dummy.next;        
+        while (p != null) {
+            q.random = map.get(p.random);            
+            p = p.next;
+            q = q.next;
+        }
+        return dummy.next;       
+    }
+}
