d814eec88cf5f6e17a4f9e779e5f3db2d7512d56
Cong Liu <drptau@gmail.com>
2018-05-05 16:35:00 -0400

    delete

--- Single Number/Solution.java
+++
@@ -1,9 +0,0 @@
-public class Solution {
-    public int singleNumber(int[] nums) {
-        int rst = 0;
-        for (int num : nums){
-            rst ^= num;
-        }
-        return rst;
-    }
-}
