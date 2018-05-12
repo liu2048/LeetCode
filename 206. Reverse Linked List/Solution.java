283f2eaaf5f9b67e17c5898167c7318979d3d31c
Cong Liu <drptau@gmail.com>
2018-05-11 23:51:37 -0400

    non-recursive solution

---
+++ 206. Reverse Linked List/Solution.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode pre, cur, nxt;
+        pre = null;
+        cur = head;
+        nxt = null;
+        while (cur != null) {
+            nxt = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = nxt;
+        }
+        return pre;
+    }
+}
