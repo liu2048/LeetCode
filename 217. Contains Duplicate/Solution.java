80e174a219ef195cd69c877312e4e82eceb7f878
Cong Liu <drptau@gmail.com>
2018-05-13 11:13:42 -0400

    !set.add(n)

--- 217. Contains Duplicate/Solution.java
+++ 217. Contains Duplicate/Solution.java
@@ -2,12 +2,15 @@ class Solution {
     public boolean containsDuplicate(int[] nums) {
         Set<Integer> set = new HashSet<>();
         for (int n : nums) {
-            if (set.contains(n)) {
-                return true;
-            } else {
-                set.add(n);
-            }
+            if (!set.add(n)) return true;              
+//             if (set.contains(n)) {
+//                 return true;
+//             } else {
+//                 set.add(n);
+//             }
         }
         return false;
     }
 }
+
+
