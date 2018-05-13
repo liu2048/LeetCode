54b0f73ace8ba53818ef08fe9f7ac591b5d1319a
Cong Liu <drptau@gmail.com>
2018-05-13 11:23:57 -0400

    redo

--- 219. Contains Duplicate II/Solution2.java
+++ 219. Contains Duplicate II/Solution2.java
@@ -1,10 +1,30 @@
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        Set<Integer> set = new HashSet<Integer>();
-        for(int i = 0; i < nums.length; i++){
-            if(i > k) set.remove(nums[i-k-1]);
-            if(!set.add(nums[i])) return true;
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (i <= k) {
+                if (set.contains(nums[i])) {
+                    return true;
+                }
+            } else {
+                set.remove(nums[i - k - 1]);
+                if (set.contains(nums[i])) {
+                    return true;
+                }
+            }
+            set.add(nums[i]);
         }
         return false;
     }
 }
+
+// class Solution {
+//     public boolean containsNearbyDuplicate(int[] nums, int k) {
+//         Set<Integer> set = new HashSet<Integer>();
+//         for(int i = 0; i < nums.length; i++){
+//             if(i > k) set.remove(nums[i-k-1]);
+//             if(!set.add(nums[i])) return true;
+//         }
+//         return false;
+//     }
+// }
