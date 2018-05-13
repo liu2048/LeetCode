e40f08aa5cbff83f7e42a323abda5c815c729550
Cong Liu <drptau@gmail.com>
2018-05-13 11:53:59 -0400

    redo

--- 207. Course Schedule/Solution.java
+++ 207. Course Schedule/Solution.java
@@ -1,35 +1,36 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         int[] inDegree = new int[numCourses];
-        List[] courses = new List[numCourses];
-        int numOfVisited = 0;        
-        for (int i = 0; i < numCourses; i++) {
-            courses[i] = new ArrayList();
-        }        
-        for (int[] edge : prerequisites) {
-            inDegree[edge[0]]++;
-            courses[edge[1]].add(edge[0]);
-        }        
+        List[] children = new List[numCourses];
         Queue<Integer> q = new LinkedList<>();
+        int visited = 0;
+        for (int i = 0; i < numCourses; i++) {
+            children[i] = new ArrayList<>();
+        }
+        for (int[] edge : prerequisites) {
+            int next = edge[0];
+            int prev = edge[1];
+            inDegree[next]++;
+            children[prev].add(next);
+        }    
         for (int i = 0; i < numCourses; i++) {
             if (inDegree[i] == 0) {
                 q.offer(i);
-                numOfVisited++;
+                visited++;
             }
-        }        
+        }
         while (!q.isEmpty()) {
             int cur = q.poll();
-            List<Integer> nextLevel = courses[cur];
-            for (int i = 0; i < nextLevel.size(); i++) {
-                int nextCur = (int) nextLevel.get(i);
-                inDegree[nextCur]--;
-                if (inDegree[nextCur] == 0) {
-                    q.offer(nextCur);
-                    numOfVisited++;
-                }                
-            }            
-        }        
-        if (numOfVisited == numCourses) return true;
-        return false;        
+            List childrenList = children[cur];
+            for (int i = 0; i < childrenList.size(); i++) {
+                int child = (int) childrenList.get(i);
+                inDegree[child]--;
+                if (inDegree[child] == 0) {
+                    q.offer(child);
+                    visited++;
+                }
+            }
+        }
+        return visited == numCourses;
     }
 }
