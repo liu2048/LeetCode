84aecc4ed92570dc5dab5460d6cf0e28263266a3
Cong Liu <drptau@gmail.com>
2018-05-14 16:20:50 -0400

    Palindrome Linked List

---
+++ 234. Palindrome Linked List/Solution.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        ListNode tail = null;
+        if (fast == null) {
+            tail = reverse(slow);
+        } else if (fast.next == null) {
+            tail = reverse(slow.next);
+        }
+        ListNode p = head, q = tail;
+        while (p != null && q != null) {
+            if (p.val != q.val) return false;
+            p = p.next;
+            q = q.next;
+        }
+        return true;
+    }
+    public ListNode reverse(ListNode node) {
+        ListNode pre, cur, nxt;
+        pre = null;
+        cur = node;
+        nxt = cur;
+        while (cur != null) {
+            nxt = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = nxt;
+        }
+        return pre;
+    }
+}
