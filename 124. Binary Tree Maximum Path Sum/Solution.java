e93d6f6524169b91cab199d7864d29b339873607
Cong Liu <drptau@gmail.com>
2018-05-15 11:26:11 -0400

    redo 2

--- 124. Binary Tree Maximum Path Sum/Solution.java
+++ 124. Binary Tree Maximum Path Sum/Solution.java
@@ -10,17 +10,18 @@
 class Solution {
     int global = Integer.MIN_VALUE;
     public int maxPathSum(TreeNode root) {
+        if (root == null) return 0;
         helper(root);
         return global;
     }
-    public int helper(TreeNode node) {
+    public int helper(TreeNode node) { 
         if (node == null) return 0;
-        int leftMax = helper(node.left);
-        int rightMax = helper(node.right);
-        int val = node.val;
-        int local_sum = Math.max(val, Math.max(val + leftMax, val + rightMax));
-        int local = Math.max(local_sum, val + leftMax + rightMax);     
-        global = Math.max(global, local);
-        return local_sum;
+        int val = node.val;   
+        int left = helper(node.left);
+        int right = helper(node.right);  
+        int localSumNode = Math.max(val, Math.max(left+val, right+val));
+        int local = Math.max(localSumNode, left + right + val);
+        this.global = Math.max(this.global, local);        
+        return localSumNode;
     }
 }
