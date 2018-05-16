ec3891e2d7f11c2f635cbe36dbb178d5991ea73f
Cong Liu <drptau@gmail.com>
2018-05-16 18:20:32 -0400

    first solution
    use map

--- 1. Two Sum/Solution.java
+++ 1. Two Sum/Solution.java
@@ -1,15 +1,18 @@
 public class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] rst = {-1, -1};
+        int[] rst = new int[]{-1, -1}; 
         if (nums == null || nums.length < 2) return rst;
-        Map<Integer, Integer> map = new HashMap<>(); // key is num[index], value is index;
+        Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++){
-            if (map.containsKey(target - nums[i])){
-                rst[0] = map.get(target - nums[i]);
+            int cur = nums[i];
+            int diff = target - cur;
+            if (map.containsKey(diff)){
+                rst[0] = map.get(diff);
                 rst[1] = i;
                 return rst;
-            }
-            map.put(nums[i], i);
+            } else {
+                map.put(cur, i);                
+            } 
         }
         return rst;
     }
