e35344960d24cec10a24caae264be9af400a2074
Cong Liu <drptau@gmail.com>
2018-05-26 16:54:52 -0400

    redo

--- 20. Valid Parentheses/Solution.java
+++ 20. Valid Parentheses/Solution.java
@@ -1,19 +1,16 @@
-public class Solution {
+class Solution {
     public boolean isValid(String s) {
-        if (s == null) return false;
         int len = s.length();
-        if (len % 2 != 0) return false;
         Stack<Character> st = new Stack<>();
-        for (int i = 0; i < len; i++){
-            char cur = s.charAt(i);
-            if (cur == '(' || cur == '[' || cur == '{'){
-                st.push(cur);
-            }
+        for (char cur : s.toCharArray()) {
+            if (cur == '(' || cur == '['|| cur == '{') st.push(cur);
             else {
                 if (st.isEmpty()) return false;
                 else {
                     char top = st.pop();
-                    if (top == '(' && cur != ')' || top == '[' && cur != ']' || top == '{' && cur != '}') return false;
+                    if (top == '(' && cur != ')') return false;
+                    if (top == '[' && cur != ']') return false;
+                    if (top == '{' && cur != '}') return false;
                 }
             }
         }
