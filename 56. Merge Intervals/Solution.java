01000c8b6b8cb4f01c4472d7e79a97da19d23598
Cong Liu <drptau@gmail.com>
2018-05-26 19:08:47 -0400

    redo 3

--- 56. Merge Intervals/Solution.java
+++ 56. Merge Intervals/Solution.java
@@ -7,31 +7,34 @@
  *     Interval(int s, int e) { start = s; end = e; }
  * }
  */
-public class Solution {
+class Solution {
     public List<Interval> merge(List<Interval> intervals) {
         List<Interval> rst = new LinkedList<>();
-        if (intervals == null || intervals.size() == 0 ) return rst;
-        Collections.sort(intervals, new myComparator());
-        Interval first_interval = intervals.get(0);
-        int start = first_interval.start;
-        int end = first_interval.end;
-        for (Interval interval : intervals){
-            if (interval.start > end){
+        if (intervals.size() == 0) return rst;
+        
+        Collections.sort(intervals, new comparator());
+        
+        Interval first = intervals.get(0);        
+        int start = first.start;
+        int end = first.end;
+        
+        for (Interval interval : intervals) {
+            if (interval.start > end) {                
                 rst.add(new Interval(start, end));
                 start = interval.start;
                 end = interval.end;
+            } else {
+                end = Math.max(end, interval.end);
             }
-            else end = Math.max(end, interval.end);
         }
         rst.add(new Interval(start, end));
         return rst;
     }
     
-    private class myComparator implements Comparator<Interval>{
+    private class comparator implements Comparator<Interval> {
         @Override
-        public int compare(Interval a , Interval b){
+        public int compare(Interval a, Interval b) {
             return a.start - b.start;
         }
     }
-    
 }
