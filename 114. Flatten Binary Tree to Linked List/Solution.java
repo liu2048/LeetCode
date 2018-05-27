6bdb558734bc6e81f36ebfa5596432d2eb6eb944
Cong Liu <drptau@gmail.com>
2018-05-26 21:58:14 -0400

    redo

--- 114. Flatten Binary Tree to Linked List/Solution.java
+++ 114. Flatten Binary Tree to Linked List/Solution.java
@@ -7,47 +7,20 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
- // preorder traversal
- public class Solution {
+class Solution {
     public void flatten(TreeNode root) {
         if (root == null) return;
         Stack<TreeNode> st = new Stack<>();
-        TreeNode pre = root;
-        if (pre.right != null) st.push(pre.right);
-        if (pre.left != null) st.push(pre.left);
-        while (!st.isEmpty()){
+        TreeNode p = root;
+        if (p.right != null) st.push(p.right);
+        if (p.left != null) st.push(p.left);
+        while (!st.isEmpty()) {
             TreeNode cur = st.pop();
-            if (cur.right != null) st.push(cur.right);
-            if (cur.left != null) st.push(cur.left);
-            pre.left = null;
-            pre.right = cur;
-            pre = cur;
-        }
-    }
-} 
- 
-// with a queue
-public class Solution {
-    public void flatten(TreeNode root) {
-        if (root == null) return;
-        Stack<TreeNode> st = new Stack<>();
-        Queue<TreeNode> q = new LinkedList<>();
-        st.push(root);
-        while (!st.isEmpty()){
-            TreeNode top = st.pop();
-            q.offer(top);
-            if (top.right != null){
-                st.push(top.right);
-            }
-            if (top.left != null){
-                st.push(top.left);
-            }
-        }
-        TreeNode node = q.poll();
-        while (!q.isEmpty()){
-            node.left = null;
-            node.right = q.poll();
-            node = node.right;
+            p.left = null;
+            p.right = cur;
+            p = cur;
+            if (p.right != null) st.push(p.right);
+            if (p.left != null) st.push(p.left);            
         }
     }
 }
