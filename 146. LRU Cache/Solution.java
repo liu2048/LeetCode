bb24b31e0835689dde0608f38c777f958388eecf
Cong Liu <drptau@gmail.com>
2018-05-27 09:52:25 -0400

    redo 2

--- 146. LRU Cache/Solution.java
+++ 146. LRU Cache/Solution.java
@@ -1,75 +1,81 @@
 class LRUCache {
+    private final int CAPACITY;    
     private Map<Integer, Node> map;
     private DoublyLinkedList dll;
-    private final int CAPACITY;    
-    
     public LRUCache(int capacity) {
         this.CAPACITY = capacity;
-        this.map = new HashMap<>(capacity);
-        this.dll = new DoublyLinkedList();
-    }    
-    public int get(int key) {
+        this.map = new HashMap<>();
+        this.dll = new DoublyLinkedList(); 
+    }
+    
+    public int get(int key) {        
         Node node = map.get(key);
-        if (node == null) return -1;        
+        if (node == null) return -1;
         dll.update(node);
-        return node.val;        
-    }    
-    public void put(int key, int value) {        
+        return node.value;
+    }
+    
+    public void put(int key, int value) {
         if (map.containsKey(key)) {
             Node node = map.get(key);
-            node.val = value;
-            dll.update(node);
+            node.value = value;
+            dll.update(node);            
         } else {
-            if (map.size() == CAPACITY) {
-                Node removedNode = dll.removeLast();
-                map.remove(removedNode.key);
-            }
             Node node = new Node(key, value);
-            dll.addFirst(node);
-            map.put(key, node);                        
-        }  
+            map.put(key, node);
+            dll.addFirst(node);            
+            if (map.size() > CAPACITY) {
+                int lastKey = dll.removeLast();
+                map.remove(lastKey);                
+            }     
+            
+        }
     }
 }
 
-class DoublyLinkedList {
-    private Node head, tail;
+class DoublyLinkedList{
+    Node head, tail;
     public DoublyLinkedList() {
-        this.head = new Node(0, 0);
-        this.tail = new Node(0, 0);
+        this.head = new Node(0,0);
+        this.tail = new Node(0,0);
         head.next = tail;
         tail.prev = head;
     }
-    public void addFirst(Node node) {
-        node.next = head.next;  
-        head.next.prev = node;
-        node.prev = head;        
-        head.next = node;
+    public void update(Node node) {
+        remove(node);
+        addFirst(node);
     }
-    public Node remove(Node node) {
-        Node prevNode = node.prev;
-        Node nextNode = node.next;
-        prevNode.next = nextNode;
-        nextNode.prev = prevNode;
+    public void remove(Node node) {
+        Node pre = node.prev;
+        Node nxt = node.next;
         node.prev = null;
         node.next = null;
-        return node;
+        pre.next = nxt;
+        nxt.prev = pre;
     }
-    public Node removeLast() {
-        return remove(tail.prev);
-    }
-    public void update(Node node) {
-        node.prev.next = node.next;
-        node.next.prev = node.prev;
-        addFirst(node);
+    public void addFirst(Node node) {
+        Node second = head.next;
+        head.next = node;
+        node.prev = head;
+        node.next = second;
+        second.prev = node;
+    }    
+    public int removeLast() {
+        Node last = tail.prev;
+        tail.prev = last.prev;
+        last.prev.next = tail;
+        last.prev = null;
+        last.next = null;
+        return last.key;
     }
 }
 
 class Node {
-    int key, val;
     Node prev, next;
-    public Node(int key, int val) {
+    int key, value;
+    public Node(int key, int value) {
         this.key = key;
-        this.val = val;
+        this.value = value;
     }
 }
 
