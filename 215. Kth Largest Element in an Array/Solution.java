78f4725cb5f8cc0796003717fdf872ae30c16170
Cong Liu <drptau@gmail.com>
2018-05-28 14:08:59 -0400

    priority queue optimized

--- 215. Kth Largest Element in an Array/Solution.java
+++ 215. Kth Largest Element in an Array/Solution.java
@@ -1,12 +1,30 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer> p = new PriorityQueue<>();
-        for(int n : nums) {
-            p.add(n);
-            if (p.size() > k) {
-                p.poll();
+        Queue<Integer> pq = new PriorityQueue<>(k);
+        for (int n : nums) {
+            if (pq.size() < k) {
+                pq.offer(n);
+            } else {
+                int top = pq.peek();
+                if (n > top) {
+                    pq.poll();
+                    pq.offer(n);
+                }
             }
         }
-        return p.poll();
+        return pq.peek();
     }
 }
+
+// class Solution {
+//     public int findKthLargest(int[] nums, int k) {
+//         Queue<Integer> pq = new PriorityQueue<>();
+//         for (int n : nums) {
+//             pq.offer(n);
+//             if (pq.size() > k) {
+//                 pq.poll();
+//             }
+//         }
+//         return pq.peek();
+//     }
+// }
