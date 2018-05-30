b67f8b20b1b8398980b410480b88c650e28888c1
Cong Liu <drptau@gmail.com>
2018-05-29 21:44:05 -0400

    HashMap + BucketSort

---
+++ 451. Sort Characters By Frequency/Solution.java
@@ -0,0 +1,37 @@
+class Solution {
+    public String frequencySort(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        int maxFre = 1;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (map.containsKey(c)) {
+                int fre = 1 + map.get(c);
+                map.put(c, fre);
+                maxFre = Math.max(maxFre, fre);
+            } else {
+                map.put(c, 1);
+            }
+        }
+        StringBuilder[] buckets = new StringBuilder[maxFre + 1];
+        for (Character c : map.keySet()) {
+            int fre = map.get(c);
+            if (buckets[fre] == null) {
+                buckets[fre] = new StringBuilder();
+            }
+            buckets[fre].append(c);
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = maxFre; i >= 0; i--) {
+            StringBuilder cur = buckets[i];
+            if (cur != null) {
+                for (int j = 0; j < cur.length(); j++) {
+                    char c = cur.charAt(j);
+                    for (int k = 0; k < i; k++) {
+                        sb.append(c);
+                    }
+                }                                
+            }            
+        }
+        return sb.toString();
+    }
+}
